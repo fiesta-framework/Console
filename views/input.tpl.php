17b5c87a3aff1a8ab76dd2442e2783a27dc3e3b0
Youssef <youssefhad2@gmail.com>
2016-05-08 14:14:13 +0100

    add form

--- views/input.tpl.php
+++ views/input.tpl.php
@@ -1,3 +1,5 @@
-<div id="input" class="input">
-	
+<div id="input_cont" class="input">
+	<form method="post" action="" id="input_form">
+		$ > <input type="text" name="Name" class="input_text" size="40">
+	</form>
 </div>
