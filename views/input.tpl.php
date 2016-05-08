735a67ed3d6b6061695f0ae52c31aaebe1ba1d7f
Youssef <youssefhad2@gmail.com>
2016-05-09 00:23:29 +0100

    remove autocomplete from input

--- views/input.tpl.php
+++ views/input.tpl.php
@@ -1,5 +1,5 @@
 <div id="input_cont" class="input">
 	<form method="post" action="" id="input_form">
-		 > $ <input type="text" name="input_text" id="input_text" class="input_text" size="40">
+		 > $ <input type="text" name="input_text" id="input_text" class="input_text" size="40" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
 	</form>
 </div>
