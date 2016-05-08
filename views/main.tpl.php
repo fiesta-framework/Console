34230a352146c45f2da4f48c7aed40fb77f3b05e
Youssef <youssefhad2@gmail.com>
2016-05-09 00:21:37 +0100

    add variable for ajax route

--- views/main.tpl.php
+++ views/main.tpl.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	
 	<title></title>
-	{Pikia\Kernel\Resources\Libs::js("app/library/jquery-1.11.3.min.js",false)}
-	{Console_Controller::css('style')}
-	{Console_Controller::js('main')}
-
+		{Pikia\Kernel\Resources\Libs::js("app/library/jquery-1.11.3.min.js",false)}
+		{Console_Controller::css('style')}
+		{Console_Controller::js('main')}
 </head>
 <body>
-	{View::import("console","input")}
+		{View::import("console","input")}
 </body>
+<script type="text/javascript">
+		var ajax_route = "{$ajax}";
+	</script>
 </html>
