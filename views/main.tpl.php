eeecccc5e2ca07f97e26f9c73b957bb4ae106f36
Youssef <youssefhad2@gmail.com>
2016-05-08 14:14:55 +0100

    call jquery and js file

--- views/main.tpl.php
+++ views/main.tpl.php
@@ -2,7 +2,10 @@
 <html>
 <head>
 	<title></title>
-	{Console_Controller::css('style.css')}
+	{Pikia\Kernel\Resources\Libs::js("app/library/jquery-1.11.3.min.js",false)}
+	{Console_Controller::css('style')}
+	{Console_Controller::js('main')}
+
 </head>
 <body>
 	{View::import("console","input")}
