3ebc3439947c7fee65b5c5ab45329a9a78e463cb
Youssef <youssefhad2@gmail.com>
2016-05-09 00:22:30 +0100

    use ajax to send data

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
 	$('#input_form').submit(function () {
-        
+		command = document.getElementById('input_text').value;
+        $.post(ajax_route,$('#input_form').serialize(),function(data)
+            {
+            	document.getElementById('input_text').value = "";
+            	$( "<div class='output'>"+data+"</div>" ).insertAfter( "#input_form" );
+            });
         //
         return false;
     });
