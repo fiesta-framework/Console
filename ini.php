95f0605bc82eb4a2df8419d28a55e05fbbc11d3a
Youssef <youssefhad2@gmail.com>
2016-05-08 10:12:51 +0100

    many updates : ini function call controllers function fetch function to fetch all controllers in controllers folder

--- ini.php
+++ ini.php
@@ -2,11 +2,38 @@
 
 namespace Pikia\Plugins\Console;
 
+use Pikia\Kernel\MVC\View\View;
+use Pikia\Kernel\Foundation\Connector;
+use Pikia\Kernel\Plugins\Plugins;
+
 
 class Console
 {
-  public static function hello()
-  {		
-  	echo "hello";
-  }
+	public static function hello()
+	{		
+		View::import("console","main");
+	}
+
+	public static function ini()
+	{		
+		
+		//
+		Connector::need();
+		View::import("console","main");
+	}
+
+	public static function callControllers()
+	{
+		$alias = "console";
+		$controllers = Plugins::getCore("console","controllers");
+		//
+		$path = "plugins/$alias/$controllers/";
+		//
+		self::call( self::fetch($path) , self::$path.'Console/' );
+	}
+
+	public static function fetch($path)
+	{
+		return Connector::fetch($path)
+	}
 }
